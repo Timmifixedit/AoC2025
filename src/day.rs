3fa73a2aa66f12ff3dea7f229f75128d34fdfe1b
Timmifixedit <tim@luchterhand.org>
2025-12-02 23:22:31 +0100

    fixed day out of bounds

--- src/day.rs
+++ src/day.rs
@@ -6,11 +6,17 @@ use thiserror::Error;
 const INPUTS: &[&'static str] = &[include_str!("../inputs/day1.txt")];
 const TEST_INPUTS: &[&'static str] = &[include_str!("../tests/day1.txt")];
 
+const fn min(a: usize, b: usize) -> usize {
+    if a < b { a } else { b }
+}
+
+const N_DAYS: usize = min(INPUTS.len(), TEST_INPUTS.len());
+
 #[derive(Error, Debug)]
 pub enum AoCError {
     #[error("Parse error: {0}")]
     ParseError(String),
-    #[error("Invalid day {0}. Must be in [1, {{INPUTS.len()}}]")]
+    #[error("Invalid day {0}. Must be in [1, {N_DAYS}]")]
     DayError(u8),
     #[error("Day {0} not implemented yet")]
     DayNotImplemented(u8),
@@ -26,18 +32,18 @@ pub trait Day {
 pub fn instantiate(day: NonZero<u8>, test_instance: bool) -> Result<Box<dyn Day>, AoCError> {
     let day = day.get();
     let day_idx = (day - 1) as usize;
+    if day_idx >= INPUTS.len() {
+        return Err(DayError(day));
+    }
+
     let data = if test_instance {
         TEST_INPUTS[day_idx]
     } else {
         INPUTS[day_idx]
     };
 
-    if day_idx >= INPUTS.len() {
-        Err(DayError(day))
-    } else {
-        match day {
-            1 => Ok(Box::new(Day1::new(data)?)),
-            _ => Err(DayNotImplemented(day)),
-        }
+    match day {
+        1 => Ok(Box::new(Day1::new(data)?)),
+        _ => Err(DayNotImplemented(day)),
     }
 }
