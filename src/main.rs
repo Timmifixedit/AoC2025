51bff641177a1687c88b3e1ddf33590a1cf2fb61
Timmifixedit <tim@luchterhand.org>
2025-12-03 00:33:20 +0100

    display lauched day

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ mod day1;
 mod day2;
 
 use crate::day::AoCError;
+use clap::Parser;
 use std::num::NonZero;
 use std::process::exit;
-use clap::Parser;
-
 
 #[derive(Parser, Debug)]
 #[command()]
@@ -15,12 +14,21 @@ struct Args {
     day: NonZero<u8>,
 
     #[arg(short, long, default_value = "false")]
-    test: bool
+    test: bool,
 }
 
 fn run() -> Result<(), AoCError> {
     let args = Args::parse();
     let day = day::instantiate(args.day, args.test)?;
+    println!(
+        "Launching day {} on {}",
+        args.day,
+        if args.test {
+            "test data"
+        } else {
+            "the actual data"
+        }
+    );
     println!("Result part 1: {}", day.part_1()?);
     println!("Result part 2: {}", day.part_2()?);
     Ok(())
