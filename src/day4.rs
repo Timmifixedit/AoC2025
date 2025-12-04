aaa931d880efd704ea4f84e3575d8f2eab934628
Timmifixedit <tim@luchterhand.org>
2025-12-04 13:54:26 +0100

    day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,6 +1,6 @@
-use crate::day::AoCError::{LogicError, ParseError};
+use crate::day::AoCError::ParseError;
 use crate::day::{AoCError, Day, Int, Res};
-use nalgebra::{DMatrix, max, min};
+use nalgebra::{max, min, DMatrix};
 
 pub struct Day4 {
     grid: DMatrix<char>,
@@ -28,6 +28,17 @@ fn matrix_iter(matrix: &DMatrix<char>, mut f: impl FnMut(usize, usize, &char)) {
     }
 }
 
+fn removable(grid: &DMatrix<char>, r: usize, c: usize) -> bool {
+    let start = (max(r as i32 - 1, 0) as usize, max(c as i32 - 1, 0) as usize);
+    let size = (
+        min(2 + (r - start.0), grid.nrows() - start.0),
+        min(2 + (c - start.1), grid.ncols() - start.1),
+    );
+    let view = grid.view(start, size);
+    let num_rolls = view.iter().filter(|&&v| v == '@').count();
+    num_rolls < 5
+}
+
 impl Day for Day4 {
     fn part_1(&self) -> Res {
         let mut num_movable = 0 as Int;
@@ -36,20 +47,32 @@ impl Day for Day4 {
                 return;
             }
 
-            let start = (max(r as i32 - 1, 0) as usize, max(c as i32 - 1, 0) as usize);
-            let size = (
-                min(2 + (r - start.0), self.grid.nrows() - start.0),
-                min(2 + (c - start.1), self.grid.ncols() - start.1),
-            );
-            let view = self.grid.view(start, size);
-            let num_rolls = view.iter().filter(|&&v| v == '@').count();
-            num_movable += (num_rolls  < 5) as Int;
+            num_movable += removable(&self.grid, r, c) as Int;
         });
 
         Ok(num_movable)
     }
 
     fn part_2(&self) -> Res {
-        todo!()
+        let mut num_removed_total: Int = 0;
+        let mut grid = self.grid.clone();
+        loop {
+            let mut num_removed: Int = 0;
+            for r in 0..grid.nrows() {
+                for c in 0..grid.ncols() {
+                    if grid[(r, c)] == '@' && removable(&grid, r, c) {
+                        grid[(r, c)] = 'x';
+                        num_removed += 1;
+                    }
+                }
+            }
+
+            num_removed_total += num_removed;
+            if num_removed == 0 {
+                break;
+            }
+        }
+
+        Ok(num_removed_total)
     }
 }
