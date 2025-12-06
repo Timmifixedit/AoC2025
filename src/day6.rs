2055882777bf2faf6531d5f6d200fd7b69eef75c
Timmifixedit <tim@luchterhand.org>
2025-12-06 13:19:16 +0100

    day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,6 @@
 use crate::day::AoCError::ParseError;
 use crate::day::{AoCError, Day, Int, Res};
+use nalgebra::DMatrix;
 
 #[derive(Debug)]
 enum Op {
@@ -32,6 +33,43 @@ struct Problem {
 
 pub struct Day6 {
     problems: Vec<Problem>,
+    problems_part_2: Vec<Problem>,
+}
+
+fn parse_part_2(input: &str, lines: Vec<&str>) -> Result<Vec<Problem>, AoCError> {
+    let mat = DMatrix::from_row_iterator(
+        lines.len() - 1,
+        lines.first().unwrap().len(),
+        input.chars().filter(|c| *c != '\n'),
+    );
+
+    let mut numbers: Vec<Vec<Int>> = Vec::new();
+    numbers.push(Vec::new());
+    for col in mat.column_iter().rev() {
+        let number = col
+            .iter()
+            .filter_map(|&c| c.to_digit(10))
+            .fold(0, |acc, n| acc * 10 + n) as Int;
+        // Only works because there are no zeros in the input
+        if number == 0 {
+            numbers.push(Vec::new());
+        } else {
+            numbers.last_mut().unwrap().push(number);
+        }
+    }
+
+    let mut problems = Vec::new();
+    for (numbers, op_str) in numbers
+        .into_iter()
+        .zip(lines.last().unwrap().split_whitespace().rev())
+    {
+        problems.push(Problem {
+            numbers,
+            op: Op::new(op_str)?,
+        })
+    }
+
+    Ok(problems)
 }
 
 impl Day6 {
@@ -61,27 +99,34 @@ impl Day6 {
                 op: Op::new(op_str)?,
             });
         }
-        Ok(Self { problems })
+
+        Ok(Self {
+            problems,
+            problems_part_2: parse_part_2(input, lines)?,
+        })
     }
+}
+
+fn grand_total(problems: &[Problem]) -> Int {
+    problems
+        .iter()
+        .map(|p| {
+            p.numbers
+                .iter()
+                .skip(1)
+                .fold(*p.numbers.first().unwrap(), |acc, &val| {
+                    p.op.apply(acc, val)
+                })
+        })
+        .sum::<Int>()
 }
 
 impl Day for Day6 {
     fn part_1(&self) -> Res {
-        Ok(self
-            .problems
-            .iter()
-            .map(|p| {
-                p.numbers
-                    .iter()
-                    .skip(1)
-                    .fold(*p.numbers.first().unwrap(), |acc, &val| {
-                        p.op.apply(acc, val)
-                    })
-            })
-            .sum::<Int>())
+        Ok(grand_total(&self.problems))
     }
 
     fn part_2(&self) -> Res {
-        todo!()
+        Ok(grand_total(&self.problems_part_2))
     }
 }
